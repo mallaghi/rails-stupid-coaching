ef2f2ea0958d01499ed04da042bdce49e65352be
maram allaghi <writtenbymaram@gmail.com>
2023-04-25 21:37:15 +0100

    built stupid coaching app

--- test/application_system_test_case.rb
+++ test/application_system_test_case.rb
@@ -1,5 +1,39 @@
 require "test_helper"
+require "application_system_test_case"
+
 
 class ApplicationSystemTestCase < ActionDispatch::SystemTestCase
-  driven_by :selenium, using: :chrome, screen_size: [1400, 1400]
+  driven_by :headless_chrome # Update this line
+
+end
+
+class QuestionsTest < ApplicationSystemTestCase
+  # [...]
+
+  test "saying Hello yields a grumpy response from the coach" do
+    visit ask_url
+    fill_in "question", with: "Hello"
+    click_on "Ask"
+
+    assert_text "I don't care, get dressed and go to work!"
+    take_screenshot
+  end
+
+  test "asking a question yields a grumpy response from the coach" do
+    visit ask_url
+    fill_in "question", with: "what's up?"
+    click_on "Ask"
+
+    assert_text "Silly question, get dressed and go to work!"
+    take_screenshot
+  end
+
+  test "telling coach your working yields a happy response" do
+    visit ask_url
+    fill_in "question", with: "I am going to work"
+    click_on "Ask"
+
+    assert_text "Great!"
+    take_screenshot
+  end
 end
